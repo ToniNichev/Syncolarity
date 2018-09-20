7bf9c3b7d154853133e919f308e5142f6785e504
Toni Nichev <toni.nichev@nbcuni.com>
2018-09-20 10:37:57 -0400

    Work

---
+++ src/SetupWindow/index.js
@@ -0,0 +1,26 @@
+const {app, BrowserWindow} = require('electron');
+
+class SetupWindow {
+
+  constructor() {
+    // Creation of the new window.
+    this.window = new BrowserWindow({
+          show: false, // Initially, we should hide it, in such way we will remove blink-effect. 
+          height: 510,
+          width: 925,
+          frame: false,  // This option will remove frame buttons. By default window has standart chrome header buttons (close, hide, minimize). We should change this option because we want to display our window like Tray Window not like common chrome-like window.
+          backgroundColor: '#E4ECEF',
+          resizable: false
+        });
+  
+    // and load the index.html of the app.
+    this.window.loadFile('./SetupWindow/index.html');
+    this.window.webContents.openDevTools()
+
+    this.window.on('blur', () => {
+      this.window.hide();
+    });    
+  }
+}
+
+module.exports = SetupWindow;
