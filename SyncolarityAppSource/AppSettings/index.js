bb26b0f9ba0098e9dab425a00f7aa81c9327da6f
Toni Nichev <toni.nichev@nbcuni.com>
2018-11-28 17:17:51 -0500

    work

---
+++ SyncolarityAppSource/AppSettings/index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+class AppSettings {
+
+  constructor(callback) {
+    this.filepath = './settings/app-settings.json';    
+    this.callback = callback;
+    this.loadSettings();
+  } 
+  
+  loadSettings() {
+    let filepath = this.filepath;
+    fs.readFile(filepath, 'utf-8', (err, data) => {
+      if(err){
+          alert("An error ocurred reading the file :" + err.message);
+          return;
+      }    
+
+      this.config = JSON.parse(data); 
+      if(typeof this.callback != 'undefined') {
+        this.callback();
+      }
+    });    
+  }
+
+  saveSettings(newConfig) {
+    this.config = newConfig;
+    let filepath = this.filepath;
+    let cfg = JSON.stringify(this.config);
+    fs.writeFile(filepath, cfg,  function (err) {
+      if(err){
+          alert("An error ocurred reading the file :" + err.message);
+          return;
+      }    
+    });     
+  }
+}  
+
+module.exports = AppSettings;
---
+++ SyncolarityAppSource/SettingsWindow/window.js
@@ -0,0 +1,94 @@
+'use strict'  
+const remote = require('electron').remote;
+const ipc = require('electron').ipcRenderer;
+let AppSettings = require('../AppSettings');
+
+/**
+ * Fires after the config file is loaded and sets up the pannels with the config data
+ */
+function configLoaded() {
+  document.querySelector('#settingsList').innerHTML = returnPanels(appSettings.config.syncConfigs.length);
+  var co = 0;
+  appSettings.config.syncConfigs.map((config) => {
+    document.querySelectorAll('#settingsList .settingsPannel')[co].querySelector('.locationHolder #sync-folder').value = config.syncFolder;    
+    addOpenFloderLocation(document.querySelectorAll('#settingsList .settingsPannel')[co].querySelector('.locationHolder #select-sync-folder'));   
+    
+    document.querySelectorAll('#settingsList .settingsPannel')[co].querySelector('#remote-server').value = config.serverUrl; 
+    document.querySelectorAll('#settingsList .settingsPannel')[co].querySelector('#exclusion-list').value = config.exclusions;
+    co ++;
+  });
+
+  document.getElementById("select-sync-folder").addEventListener("click", function (e) {
+    var dialog = remote.dialog;
+    var selection = dialog.showOpenDialog({ properties: ['openDirectory']})
+  
+    if (selection && selection[0]) {
+      console.log('got Selection');
+    }
+    syncFolderPath = selection[0];
+    document.getElementById("sync-folder").value = syncFolderPath;
+  }); 
+}
+
+function addOpenFloderLocation(e) {
+  e.addEventListener('click', function(e){
+    var dialog = remote.dialog;
+    var selection = dialog.showOpenDialog({ properties: ['openDirectory']})
+  
+    if (selection && selection[0]) {
+      console.log('got Selection');
+    }
+    e.currentTarget.parentElement.querySelector('#sync-folder').value = selection[0];
+  });    
+}
+
+/**
+ * adding new settings pannel
+ */
+function addNewSettingsPanel() {
+  var element = document.createElement('div');
+  element.innerHTML = returnPanels(1);
+  document.querySelector("#settingsList").appendChild(element);
+  var last = document.querySelectorAll('#settingsList .settingsPannel').length - 1;
+  addOpenFloderLocation(document.querySelectorAll('#settingsList .settingsPannel')[last].querySelector('.locationHolder #select-sync-folder'));
+}
+
+let appSettings = new AppSettings(configLoaded);
+
+
+/**
+ * SAVE buton clicked
+ */
+document.getElementById("save").addEventListener("click", function (e) {
+  var co = 0;
+  var len = document.querySelectorAll('#settingsList .settingsPannel').length;
+  for(var co = 0; co < len ;co ++) {
+    var config = {};
+    config.syncFolder = document.querySelectorAll('#settingsList .settingsPannel')[co].querySelector('.locationHolder #sync-folder').value;    
+    config.serverUrl = document.querySelectorAll('#settingsList .settingsPannel')[co].querySelector('#remote-server').value; 
+    config.exclusions = document.querySelectorAll('#settingsList .settingsPannel')[co].querySelector('#exclusion-list').value;
+    appSettings.config.syncConfigs.push(config);
+  }  
+  appSettings.saveSettings(appSettings.config);
+});
+
+
+
+ipc.on('update-config', (event, AppSettings) => {
+  /*
+  debugger;
+  //appSettings = AppSettings;
+  document.getElementById("sync-folder").value = appSettings.config.syncFolder;
+  document.getElementById("remote-server").value = appSettings.config.syncFolder;
+  document.getElementById("exclusion-list").value = appSettings.config.exclusinList;
+  */
+});
+
+
+function returnPanels(numberPanels) {
+  let html = '';
+  for(var q = 0;q < numberPanels; q++) {
+    html += document.querySelector('#panelsContainer > .settingsPannel').outerHTML;
+  }
+  return html;
+}
